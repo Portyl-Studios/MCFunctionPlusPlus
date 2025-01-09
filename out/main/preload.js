603a4a4e06c33150f07cf2bc73d2d9bc0863c1d1
Wen Loong <wenloong@portylstudios.com>
2025-01-09 09:01:28 +0800

    untrack out folder

--- out/main/preload.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// Use CommonJS
-const { contextBridge } = require('electron');
-//import { contextBridge } from 'electron'
-contextBridge.exposeInMainWorld('electron', {
-// Add APIs if needed
-});
